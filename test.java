f0064456dd21eea315721b27f85d6c4cbfe7ce4e
mdashif5@gmail.com <55871288+mdashifreza@users.noreply.github.com>
2023-02-14 14:01:55 +0530

    commandline input print

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++)
+        {
+            System.out.print(args[i]+" ");
+        }
+    }
+}
